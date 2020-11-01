4d22e07b5dbb0d7d50b1850072512495c6b4bd67
rom201 <kovalchukr1104@gmail.com>
2020-11-01 10:55:42 -0500

    batmen sayse more

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -2,5 +2,6 @@ public class Batman {
     public static void main(String[] args) {
         System.out.println("Batman file action 1");
         System.out.println("I will catch joker");
+        System.out.println("I will catch joker 2 ");
     }
 }
